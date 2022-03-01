933ca891b884491149e4998d0c0d1357139318a0
JoaoVP03 <100729058+JoaoVP03@users.noreply.github.com>
2022-03-01 18:48:44 -0300

    Add files via upload

---
+++ js/anima.js
@@ -0,0 +1,15 @@
+$('.banner').slick({
+  dots: true,
+  slidesToShow: 1,
+  autoplaySpeed:2000,
+  autoplay: true,
+  cssEase: 'linear',
+  arrows: true, 
+});
+
+$('.galeria').slick({
+  slidesToShow: 10,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
